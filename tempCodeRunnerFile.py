12b30d94f79b406866f494d8f6c67eeffdf1da50
Việt Nam <123968522+VietNam0410@users.noreply.github.com>
2025-02-19 11:18:05 +0700

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+import mlflow
+# import mlflow.sklearn
+# import joblib
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+# from sklearn.model_selection import train_test_split, cross_val_score
+# from sklearn.ensemble import RandomForestClassifier
+# from sklearn.metrics import accuracy_score
